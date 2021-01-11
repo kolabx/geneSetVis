fc82e2a2e581e40497b61d3aa1fc8b6e91d76d85
Kosiso Onwuzu <kosisoonwuzu@Kosisos-MacBook-Air.local>
2021-01-11 01:54:02 -0800

    string11 changes

--- inst/app/modules/stringdb.R
+++ inst/app/modules/stringdb.R
@@ -193,7 +193,7 @@ stringdbModule <- function(session, input, output, envir, appDiskCache) {
 	      'Genes in Term' = preferredNames
 	    )
 	    
-	  #table$'Term Description' <- gsub(pattern = 'GO.', replacement = 'GO:', x = table$'Term Description')
+	  table$'Term Description' <- gsub(pattern = 'GO.', replacement = 'GO:', x = table$'Term Description')
 
 	  makeTermsTable(
 	    table = table,
